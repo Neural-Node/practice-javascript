5ac9347c6b1a7f0d59f46eda112fca3b5bf06196
Jesse Javana <jjavana@ewu.edu>
2023-05-18 22:41:52 -0700

    Functions File with Looping added

--- LearingJavaScript/FunctionsAndStuff.js
+++ LearingJavaScript/FunctionsAndStuff.js
@@ -10,3 +10,34 @@ if(true){
 }
 // y is not visible here
 console.log(x + z);
+
+//If you write an = operator after a parameter, followed by an expression the value of that expression will
+// will replace the arguement when it is not give.
+function power(base, exponent = 2){
+    let result = 1;
+    for(let count = 0; count < exponent; count++){
+        result += base;
+    }
+    return result;
+}
+
+// function call
+console.log(power(4));
+console.log(power(2,6));
+
+//This function asks for a function of two arguments- the number of cows and the number of chickens
+function printFarmInventory(cows,chickens){
+    let cowString = String(cows);
+    while (cowString.length < 3){
+        cowString = "0" + cowString;
+    }
+    console.log(`${cowString} Cows`)
+    let chickenString = String(chickens);
+    while (chickenString.length < 3){
+        chickenString = "0"+chickenString;
+    }
+    console.log(`${chickenString} Chickens`)
+}
+
+// call printFarmInventory
+printFarmInventory(7,11)
