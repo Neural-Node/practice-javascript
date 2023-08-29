21dcad0c4ad00ef601527acc10a40c89577e69d2
Jesse Javana <jjavana@ewu.edu>
2023-08-28 22:45:25 -0700

    fix: add Celcius to Farenheit

---
+++ LearingJavaScript/CelciusToFarenheit.js
@@ -0,0 +1,9 @@
+//Program to convert celcius to farenheit
+//ask the celcius to value to the user
+const celcius = prompt("Enter a Celcius value : ");
+
+//Calculate farenheit
+const farenheit =(celcius * 1.8) + 32
+
+//display result
+console.log(`${celcius} degree celcius is equal to ${farenheit} degree farenheit. `);
